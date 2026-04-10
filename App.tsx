53539c34dc2c7b71985eb78101c589bf4cf834e1
TerminalNomad <zach@terminalnomad.com>
2026-04-10 16:11:46 -0400

    Add files via upload

--- App.tsx
+++ App.tsx
@@ -58,14 +58,25 @@ const TikTokIcon = React.forwardRef(({ color = 'currentColor', size = 24, stroke
 });
 
 // Custom Snapchat Icon
-const SnapchatIcon = React.forwardRef<svgsvgelement, lucideprops="">(({ color = 'currentColor', size = 24, strokeWidth = 2, ...props }, ref) => {
+const SnapchatIcon = React.forwardRef(({ color = 'currentColor', size = 24, strokeWidth = 2, ...props }: any, ref: any) => {
   return (
-    <svg ref="{ref}" {...props}="" xmlns="http://www.w3.org/2000/svg" width="{size}" height="{size}" viewbox="0 0 24 24" fill="none" stroke="{color}" strokewidth="{strokeWidth}" strokelinecap="round" strokelinejoin="round">
+    <svg
+      ref={ref}
+      {...props}
+      xmlns="http://www.w3.org/2000/svg"
+      width={size}
+      height={size}
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke={color}
+      strokeWidth={strokeWidth}
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    >
       <path d="M12 2.5c-2.3 0-5.5 1.7-5.5 6 0 2.5 1.2 3.6 1.2 5 0 1.2-1 1.7-1.7 1.7-1 0-1.5.8-1.5 1.8 0 1.8 2.2 2.5 3.5 2.5.5 0 1 .5 1 1 0 .6-.7 1-1.5 1-.3 0-.5.2-.5.5 0 .5 1 .5 2 .5 1.5 0 3-.2 5-.5 2 .3 3.5.5 5 .5 1 0 2 0 2-.5 0-.3-.2-.5-.5-.5-.8 0-1.5-.4-1.5-1 0-.5.5-1 1-1 1.3 0 3.5-.7 3.5-2.5 0-1-.5-1.8-1.5-1.8-.7 0-1.7-.5-1.7-1.7 0-1.4 1.2-2.5 1.2-5 0-4.3-3.2-6-5.5-6z"/>
     </svg>
   );
 });
-
 const LINKS: LinkItem[] = [
   // Socials
   {
