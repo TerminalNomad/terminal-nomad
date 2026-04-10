c303a8f61e6707e147daf1a187212f3ca13100d4
TerminalNomad <zach@terminalnomad.com>
2026-04-10 16:10:09 -0400

    Add files via upload

--- App.tsx
+++ App.tsx
@@ -37,9 +37,21 @@ const PaypalIcon = React.forwardRef<svgsvgelement, lucideprops="">(({ color = 'c
 });
 
 // Custom TikTok Icon
-const TikTokIcon = React.forwardRef<svgsvgelement, lucideprops="">(({ color = 'currentColor', size = 24, strokeWidth = 2, ...props }, ref) => {
+const TikTokIcon = React.forwardRef(({ color = 'currentColor', size = 24, strokeWidth = 2, ...props }: any, ref: any) => {
   return (
-    <svg ref="{ref}" {...props}="" xmlns="http://www.w3.org/2000/svg" width="{size}" height="{size}" viewbox="0 0 24 24" fill="none" stroke="{color}" strokewidth="{strokeWidth}" strokelinecap="round" strokelinejoin="round">
+    <svg
+      ref={ref}
+      {...props}
+      xmlns="http://www.w3.org/2000/svg"
+      width={size}
+      height={size}
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke={color}
+      strokeWidth={strokeWidth}
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    >
       <path d="M9 12a4 4 0 1 0 4 4V4a5 5 0 0 0 5 5"/>
     </svg>
   );
